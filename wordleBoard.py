8153f6aa3018712752e2fc03787e4e9c252de44b
LiamMcBride <mailmcbride56@gmail.com>
2022-01-30 15:31:49 -0500

    Working

--- wordleBoard.py
+++ wordleBoard.py
@@ -1,20 +1,128 @@
+import os
+import random
+
 class WordleBoard:
     def __init__(self):
-        self.board = [[],[],[],[],[]]
-        self.word = "abort"
+        self.board = [
+            ["-","-","-","-","-"],
+            ["-","-","-","-","-"],
+            ["-","-","-","-","-"],
+            ["-","-","-","-","-"],
+            ["-","-","-","-","-"]
+            ]
+        self.scoreBoard = [
+            ["i","i","i","i","i"],
+            ["i","i","i","i","i"],
+            ["i","i","i","i","i"],
+            ["i","i","i","i","i"],
+            ["i","i","i","i","i"]
+            ]
+        self.bank = [
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "w",
+            "y",
+            "x",
+            "z"
+        ]
+        self.word = self.getWord()
+        self.guess = 0
 
     def printBoard(self):
         boardString = ''
-        for line in self.board:
-            for letter in line:
+        for i in range(0,5):
+            for letter in self.board[i]:
+                boardString += letter
+            boardString += "\n"
+            for letter in self.scoreBoard[i]:
                 boardString += letter
             boardString += "\n"
         print(boardString)
 
     def takeGuess(self):
         resp = input("Please enter a guess:\n")
-        self.board[0] = list(resp)
+        if self.checkInBank(resp):
+            self.board[self.guess] = list(resp)
+            self.guess += 1
+        else:
+            print("Not a word")
+            self.takeGuess()
+    
+    def checkInBank(self, word):
+        words = []        
+        with open('cleanDict.txt') as f:
+            lines = f.readlines()
+            for line in lines:
+                words.append(line[0:-1])
+        f.close()
+        return word in words
+
+    def verifyGuess(self):
+        # if(self.word == self.createWord()):
+        #     print("correct")
+        # else:
+        for i in range(0,5):
+            let = self.board[self.guess - 1][i]
+            if let == self.word[i]:
+                self.scoreBoard[self.guess - 1][i] = "C"
+            elif let in self.word:
+                self.scoreBoard[self.guess - 1][i] = "O"
+            else:
+                self.scoreBoard[self.guess - 1][i] = "X"
+                if let in self.bank:
+                    self.bank.remove(let)
+                
+
+    def createWord(self):
+        word = ""
+        for let in self.board[self.guess - 1]:
+            word += let
+        return word
+
+    def getWord(self):
+        words = []
+        with open('cleanDict.txt') as f:
+            words = f.readlines()
+        f.close()
+
+        word = random.choice(words)
+
+        return word[0:-1]
+
+    def showWordBank(self):
+        line = "\n\n"
+        for let in self.bank:
+            line += let + " "
+        print(line)
+
+
 
 board = WordleBoard()
+for i in range(0,5):
+    board.takeGuess()
+    board.verifyGuess()
+    os.system('cls')
+    board.printBoard()
+    board.showWordBank()
 
-board.printBoard()
+print(board.word)
